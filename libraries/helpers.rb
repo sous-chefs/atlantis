d1b06ba907e0681ff41cd76424ac66a0b76b4c0b
Dan Webb <dan.webb@damacus.io>
2023-10-03 19:48:06 +0100

    Update centos to centos-stream-8
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -3,16 +3,30 @@
 module AtlantisCookbook
   module Helpers
     def cpu_arch
-      if node['kernel']['processor'].match?(/x86_64/)
+      case node['kernel']['processor']
+      when /x86_64/
         'amd64'
+      when /armv8/
+        'arm64'
       else
         '386'
       end
     end
 
+    def platform
+      case node['platform']
+      when 'windows'
+        'windows'
+      when 'mac_os_x'
+        'darwin'
+      else
+        'linux'
+      end
+    end
+
     def github_download_url(base_url, version)
       # https://github.com/runatlantis/atlantis/releases/download/v0.4.4/atlantis_linux_amd64.zip
-      "#{base_url}/v#{version}/atlantis_linux_#{cpu_arch}.zip"
+      "#{base_url}/v#{version}/atlantis_#{platform}_#{cpu_arch}.zip"
     end
 
     def hashicorp_download_url(base_url, product, version)
--- resources/terragrunt_installer.rb
+++ resources/terragrunt_installer.rb
@@ -7,6 +7,7 @@ unified_mode true
 
 property :checksum, String, regex: /^[a-zA-Z0-9]{64}$/, default: '3b033389977ca6e7d10bad10514f22fa767c85b76db92befe83e67bafa2c8413'
 property :download_base_url, String, default: 'https://github.com/gruntwork-io/terragrunt/releases/download'
+property :ignore_checksum, [true, false], default: false
 
 property :group, [String, Integer], default: 'root'
 property :owner, [String, Integer], default: 'root'
@@ -19,8 +20,7 @@ default_action :install
 action :install do
   remote_file '/usr/local/bin/terragrunt' do
     source terragrunt_github_download_url(new_resource.download_base_url, new_resource.version)
-    checksum new_resource.checksum unless new_resource.checksum.nil?
-
+    checksum new_resource.checksum unless new_resource.ignore_checksum
     owner new_resource.owner
     group new_resource.group
     mode new_resource.mode
